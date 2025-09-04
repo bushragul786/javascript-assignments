3d4de62eca46dfaea0193d920a54052d3c321804
bushragul786 <bushragul427@gmail.com>
2025-09-05 02:35:42 +0500

    chapter no 2 Assignment done

---
+++ chapter#2/app.js
@@ -0,0 +1,92 @@
+//question # 1
+//Declare a variable called username.
+
+//answer
+var username;
+console.log(username)
+
+//question # 2
+//2. Declare a variable called myName & assign to it a string that represents your Full Name.
+var username = "bushragul";
+console.log(username)
+
+//question # 3
+//3. Write script to
+//a) Declare a JS variable, titled message.
+//b) Assign “Hello World” to variable message
+//c) Display the message in alert box.
+
+
+// a) Declare a JS variable, titled message.
+//answer
+var message;
+
+// b) Assign "Hello World" to variable message
+message = "Hello World";
+
+// c) Display the message in alert box.
+alert(message);
+
+//question #4 
+//4. Write a script to save student’s bio data in JS variables and show the data in alert boxes.
+
+//answer
+// Student's Bio Data Variables
+var studentName = "Jhone doe";
+var age = 15;
+var certified = "Certified mobile Application Development"
+
+// Show data in alert boxes
+alert("Student Name: " + studentName);
+alert("Age: " + age);
+alert("certified:" + certified)
+
+
+//question # 5
+//5. Write a script to display the following alert using one JS variable:
+
+//answer
+var food = "pizza\npizz\npiz\npi\np";
+
+// Show in alert
+alert(food);
+
+
+//question # 6
+// 1. Declare a variable called email
+//answer
+var email = "bushragul@example.com";
+
+// 2. Show message using string concatenation
+alert("My email address is " + email);
+
+
+//question#7
+// Declare a variable called book
+//answer
+var book = "A smarter way to learn JavaScript";
+
+// Show the message in alert box
+alert("I am trying to learn from the book " + book);
+
+//question#8
+//answer
+//8. Write a script to display this in browser through JS
+document.write("Yah! I can write HTML content through javascript <br> ")
+
+//question#9
+//answer
+// Store string in a variable
+var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬" ;
+ var heart = "\u2665";
+
+// Show in alert
+alert(design);
+alert(heart)
+
+// Show in browser
+   document.write(design + "<br>");
+    document.write(heart + "<br>");
+
+// show in browser on page
+ document.write("-- END -- <br>");
